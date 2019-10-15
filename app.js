1729b717b63762212f917e2980e6282e9eec334d
Eric Franco <francoeric@gmail.com>
2019-10-16 01:30:24 +0200

    Added endpoints for filtering clients by ID or Name

--- app.js
+++ app.js
@@ -21,6 +21,52 @@ server.get("/clients", (req, res) => {
     });
 })
 
+//Get client data filtered by client Id
+server.get("/clientID/:id", (req, res) => {
+    let _id = req.params.id
+
+    request('http://www.mocky.io/v2/5808862710000087232b75ac', (error, response, body) => {
+        if (error) throw error
+        else {
+            let allClients = JSON.parse(body).clients
+            let clientData = allClients.filter(client => client["id"] === _id)[0];
+
+            if (clientData) {
+                res.status(200).send({
+                    "data": clientData
+                })
+            } else {
+                res.status(404).send({
+                    "message": "Client not found. Please check the ID is correct"
+                })
+            }
+        }
+    });
+})
+
+//Get client data filtered by client name
+server.get("/clientName/:name", (req, res) => {
+    let _name = req.params.name
+
+    request('http://www.mocky.io/v2/5808862710000087232b75ac', (error, response, body) => {
+        if (error) throw error
+        else {
+            let allClients = JSON.parse(body).clients
+            let clientData = allClients.filter(client => client["name"] === _name)[0];
+
+            if (clientData) {
+                res.status(200).send({
+                    "data": clientData
+                })
+            } else {
+                res.status(404).send({
+                    "message": "Client not found. Please check the name is correct"
+                })
+            }
+        }
+    });
+})
+
 //Get list of all policies
 server.get("/policies", (req, res) => {
 
