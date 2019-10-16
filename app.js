c922581d2605d2dd5471a449f6045a1e285c5fb7
Eric Franco <francoeric@gmail.com>
2019-10-16 10:10:58 +0200

    Added endponts for policies and clients links

--- app.js
+++ app.js
@@ -121,6 +121,112 @@ server.get("/policies", (req, res) => {
     });
 })
 
+//Get list of policies linked to a client name
+server.get("/policiesOfClient/:clientName", (req, res) => {
+
+    let authToken = req.headers["authorization"];
+    authToken = authToken.replace("Bearer ", "")
+
+    jwt.verify(authToken, "jwtpassword", (err, decoded) => {
+        if (err) throw err
+        else {
+            //User is valid, but only admin can proceed
+            if (decoded["role"] != "admin") {
+                res.status(401).send({
+                    "message": "Unauthorized user"
+                })
+            } else {
+                request('http://www.mocky.io/v2/5808862710000087232b75ac', (error, response, clientsBody) => {
+                    if (error) throw error
+                    else {
+                        let allClients = JSON.parse(clientsBody).clients;
+                        let _name = req.params.clientName;
+                        let clientData = allClients.filter(client => client["name"] === _name)[0];
+
+                        if (clientData) {
+                            request('http://www.mocky.io/v2/580891a4100000e8242b75c5', (error, response, policiesBody) => {
+                                if (error) throw error
+                                else {
+                                    let allPolicies = JSON.parse(policiesBody).policies;
+                                    let policiesOfUser = allPolicies.filter(policy => policy["clientId"] === clientData["id"]);
+
+                                    if (policiesOfUser.length == 0) {
+                                        //That client has no policies linked to them
+                                        res.status(404).send({
+                                            "message": "No policies linked to this client were found. Please check the name is correct"
+                                        })
+                                    } else {
+                                        res.status(200).send({
+                                            "data": policiesOfUser
+                                        })
+                                    }
+                                }
+                            });
+                        } else {
+                            res.status(404).send({
+                                "message": "Client not found. Please check the name is correct"
+                            })
+                        }
+                    }
+                });
+            }
+        }
+    })
+})
+
+//Get client data linked to a policy id
+server.get("/policyClient/:policyId", (req, res) => {
+    let authToken = req.headers["authorization"];
+    authToken = authToken.replace("Bearer ", "")
+
+    jwt.verify(authToken, "jwtpassword", (err, decoded) => {
+        if (err) throw err
+        else {
+
+            //User is valid, but only admin can proceed
+            if (decoded["role"] != "admin") {
+                res.status(401).send({
+                    "message": "Unauthorized user"
+                })
+            } else {
+                request('http://www.mocky.io/v2/580891a4100000e8242b75c5', (error, response, policiesBody) => {
+
+                    if (error) throw error
+                    else {
+                        let allPolicies = JSON.parse(policiesBody).policies
+                        let _id = req.params.policyId;
+                        let policyData = allPolicies.filter(policy => policy["id"] === _id)[0];
+
+                        if (policyData) {
+                            request('http://www.mocky.io/v2/5808862710000087232b75ac', (error, response, clientsBody) => {
+                                if (error) throw error
+                                else {
+                                    let allClients = JSON.parse(clientsBody).clients
+                                    let clientData = allClients.filter(client => client["id"] === policyData["clientId"])[0];
+                                    if (clientData) {
+                                        res.status(200).send({
+                                            "data": clientData
+                                        })
+                                    } else {
+                                        //That policy has no clients linked to them
+                                        res.status(404).send({
+                                            "message": "No clients linked to this policy were found. Please check the policy Id is correct"
+                                        })
+                                    }
+                                }
+                            })
+                        } else {
+                            res.status(404).send({
+                                "message": "Policy not found. Please check the policy Id is correct"
+                            })
+                        }
+                    }
+                })
+            }
+        }
+    })
+})
+
 server.listen(3000, () => {
     console.log('Listening on port 3000')
 })
