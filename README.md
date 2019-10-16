c66cbecbe46aebfeba77d2e46c725eaa67b92fc2
Eric Franco <francoeric@gmail.com>
2019-10-16 12:36:42 +0200

    Updated readme and error catching

--- README.md
+++ README.md
@@ -12,6 +12,8 @@ Web API that manages some information regarding insurance policies and company c
 
 **4) Body-parser:** middleware to parse bodies of incoming requests
 
+**5) jsonwebtoken:** library that allos us the creation and verification of authorization tokens
+
 ## How to use this repository
 
 1) Clone or download repository
@@ -24,9 +26,56 @@ Web API that manages some information regarding insurance policies and company c
 
 ## Additional considerations
 
-- (Will be updated)
+- In order to get the information necessary for those endpoints that require parameters, we included 2 general endpoints that returns us all clients or policies information.
+- For the JWT to work, we need a secret password that is used for both signing and verification. This password should be in a `secrets.json` file, but for ease the testing of this assessment, a basic password `jwtpassword` was included in the code. This should be replaced by the password in the secrets.json file, and this file read and parsed using `filesystem' read synchronous function.
 
 # Endpoints
+
+## **Login**
+
+* **URL**
+
+    /login
+
+* **Method:**
+
+  `POST`
+
+* **Authorization**
+
+    none 
+
+* **Body**
+
+    Only user email is required for login in this assessment.
+    - Example: `{"email": "manningblankenship@quotezart.com"}`
+
+* **Success Response:**
+    - **Code:** 200
+    - Response example:
+    ```
+        {
+        "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImJyaXRuZXlibGFua2Vuc2hpcEBxdW90ZXphcnQuY29tIiwiaWQiOiJhMGVjZTVkYi1jZDE0LTRmMjEtODEyZi05NjY2MzNlN2JlODYiLCJuYW1lIjoiQnJpdG5leSIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTU3MTIxNzY0NX0.e5kRqYttMmTHTorTC39PkPlivI5yFAA3D7oi-RuEJGU"
+        }
+    ```
+
+    - This token will be used in the headers of our request in order to get access to 2 of the endpoints in this assessment.
+
+* **Error Response:**
+
+    *Email is not provided in the body*
+    - **Code:** 400
+    - **Content:**
+    ```
+        "message": "No email address was provided."
+    ```
+
+    *Email is not one from the clients list*
+    - **Code:** 400
+    - **Content:**
+    ```
+        "message": "Email provided is not valid."
+    ```
 
 ## **Clients**
 
@@ -89,7 +138,7 @@ Web API that manages some information regarding insurance policies and company c
    `id = [string]`
 
    Example:
-   ``/client/a3b8d425-2b60-4ad7-becc-bedf2ef860bd``
+   `/client/a3b8d425-2b60-4ad7-becc-bedf2ef860bd`
 
 * **Success Response:**
     - **Code:** 200
@@ -107,7 +156,7 @@ Web API that manages some information regarding insurance policies and company c
  - **Code:** 404
     - **Content:**
     ```
-        "message": "Client not found. Please check the ID is correct"
+        "message": "Client not found. Please check the ID is correct."
     ```
 
 
@@ -134,10 +183,10 @@ Web API that manages some information regarding insurance policies and company c
 
    **Required:**
 
-   `id = [string]`
+   `name = [string]`
 
    Example:
-   ``/client/Barnett``
+   `/client/Barnett`
 
 * **Success Response:**
     - **Code:** 200
@@ -155,15 +204,255 @@ Web API that manages some information regarding insurance policies and company c
     - **Code:** 404
     - **Content:**
     ```
-        "message": "Client not found. Please check the name is correct"
+        "message": "Client not found. Please check the name is correct."
     ```
 
+
+## **Policies**
+
+* **URL**
+
+    /policies
+
+* **Method:**
+
+  `GET`
+
+* **Authorization**
+
+    none 
+
+* **Body**
+
+    none
+
+* **Success Response:**
+    - **Code:** 200
+    - Response example:
+    ```
+        [{
+        "id":"e8fd159b-57c4-4d36-9bd7-a59ca13057bb",
+        "name":"Manning",
+        "email":"manningblankenship@quotezart.com",
+        "role":"admin"
+        },
+        {
+        "id":"a3b8d425-2b60-4ad7-becc-bedf2ef860bd",
+        "name":"Barnett",
+        "email":"barnettblankenship@quotezart.com",
+        "role":"user"
+        }]
+    ```
+
+## **Get list of policies linked to a client name**
+
+* **URL**
+
+    /policiesOfClient/:clientName
+
+* **Method:**
+
+  `GET`
+
+* **Authorization**
+
+    Only users with the role `admin` can get information from this endpoing
+
+    To get this information, an authorization token has to be included in the headers of the request
+
+    - *HEADERS*
+        - `KEY`: Authorization
+        - `VALUE`: Bearer + /blank_space/ + /login_token/
+
+    - Example:
+        ```
+            {'Authorization': Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImJyaXRuZXlibGFua2Vuc2hpcEBxdW90ZXphcnQuY29tIiwiaWQiOiJhMGVjZTVkYi1jZDE0LTRmMjEtODEyZi05NjY2MzNlN2JlODYiLCJuYW1lIjoiQnJpdG5leSIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTU3MTE3MTc5Mn0.mNeDw5DTU-19o0c3ke_3TEUOBxHzqRoIebU48RfvRl8
+            }
+        ```
+        
+        - This token is from a user that has the `admin` role, so it can be used to get information from this endpoint.
+
+* **Body**
+
+    none
+
+*  **URL Params**
+
+   **Required:**
+
+   `clientName = [string]`
+
+   Example:
+   `/policiesOfClient/Britney`
+
+* **Success Response:**
+    - **Code:** 200
+    - **Content:**
+    ```        
+        {
+            "data": [
+            {
+                "id": "7b624ed3-00d5-4c1b-9ab8-c265067ef58b",
+                "amountInsured": 399.89,
+                "email": "inesblankenship@quotezart.com",
+                "inceptionDate": "2015-07-06T06:55:49Z",
+                "installmentPayment": true,
+                "clientId": "a0ece5db-cd14-4f21-812f-966633e7be86"
+            },
+            (...)
+            {
+                "id": "0df3bcef-7a14-4dd7-a42d-fa209d0d5804",
+                "amountInsured": 705.14,
+                "email": "inesblankenship@quotezart.com",
+                "inceptionDate": "2014-05-11T12:28:41Z",
+                "installmentPayment": false,
+                "clientId": "a0ece5db-cd14-4f21-812f-966633e7be86"
+            }
+            ]
+        }
+    ```
+
+* **Error Response:**
+
+    *Token is not provided*
+    - **Code:** 401
+    - **Content:**
+    ```
+        "message": "Unauthorized user. Authentication token is required."
+    ```
+
+    *Token provided is not valid*
+    - **Code:** 401
+    - **Content:**
+    ```
+        "message": "Unauthorized user. Token provided is not valid."
+    ```
+
+    *Token provided is not from a user with `admin` role.*
+    - **Code:** 401
+    - **Content:**
+    ```
+        "message": "Unauthorized user."
+    ```
+
+    *Name of the client in the url is not correct*
+    - **Code:** 404
+    - **Content:**
+    ```
+        "message": "Client not found. Please check the name is correct."
+    ```
+
+    *Client selected has no policies linked*
+    - **Code:** 404
+    - **Content:**
+    ```
+        "message": "No policies linked to this client were found. Please check the name is correct."
+    ```
+
+
+
+## **Get client data linked to a policy id**
+
+* **URL**
+
+    /policyClient/:policyId
+
+* **Method:**
+
+  `GET`
+
+* **Authorization**
+
+    Only users with the role `admin` can get information from this endpoing
+
+    To get this information, an authorization token has to be included in the headers of the request
+
+    - *HEADERS*
+        - `KEY`: Authorization
+        - `VALUE`: Bearer + /blank_space/ + /login_token/
+
+    - Example:
+        ```
+            {'Authorization': Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImJyaXRuZXlibGFua2Vuc2hpcEBxdW90ZXphcnQuY29tIiwiaWQiOiJhMGVjZTVkYi1jZDE0LTRmMjEtODEyZi05NjY2MzNlN2JlODYiLCJuYW1lIjoiQnJpdG5leSIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTU3MTE3MTc5Mn0.mNeDw5DTU-19o0c3ke_3TEUOBxHzqRoIebU48RfvRl8
+            }
+        ```
+        
+        - This token is from a user that has the `admin` role, so it can be used to get information from this endpoint.
+
+* **Body**
+
+    none
+
+*  **URL Params**
+
+   **Required:**
+
+   `policyId = [string]`
+
+   Example:
+   `/policyClient/64cceef9-3a01-49ae-a23b-3761b604800b`
+
+* **Success Response:**
+    - **Code:** 200
+    - **Content:**
+    ```        
+        {
+            "data": {
+                "id": "e8fd159b-57c4-4d36-9bd7-a59ca13057bb",
+                "name": "Manning",
+                "email": "manningblankenship@quotezart.com",
+                "role": "admin"
+            }
+        }
+    ```
+
+* **Error Response:**
+
+    *Token is not provided*
+    - **Code:** 401
+    - **Content:**
+    ```
+        "message": "Unauthorized user. Authentication token is required."
+    ```
+    *Token provided is not valid*
+    - **Code:** 401
+    - **Content:**
+    ```
+        "message": "Unauthorized user. Token provided is not valid."
+    ```
+
+    *Token provided is not from a user with `admin` role.*
+    - **Code:** 401
+    - **Content:**
+    ```
+        "message": "Unauthorized user."
+    ```
+
+    *Name of the client in the url is not correct*
+    - **Code:** 404
+    - **Content:**
+    ```
+        "message": "Policy not found. Please check the policy Id is correct."
+    ```
+
+    *Client selected has no policies linked*
+    - **Code:** 404
+    - **Content:**
+    ```
+        "message": "No clients linked to this policy were found. Please check the policy Id is correct."
+    ```
 
 # Possible improvements
 
 As we were provided with 2 different urls which contain all the data necessary to make this API work, we used http calls to get all that data.
 
-Best possible improvement would be the use of a database, specially MongoDB, as the information we already have is in JSON format.
+Therefore, first improvement would be to access this data without a request call.
+
+Easiest way could be saving that data into two JSON files and access using the `filesystem` *read* function, to asynchronally read this information from the files.
+
+- Having all the data in files can also be interesting for testing purposes, as we can access all Id's and names in order to test the endpoints in an easier way.
+
+Best possible improvement would be the use of a database, specially `MongoDB`, as the information we already have is in JSON format.
 
 1) Create a Schema for Clients and Policies
 
--- app.js
+++ app.js
@@ -18,7 +18,7 @@ server.post('/login', (req, res) => {
     if (loginEmail == undefined || loginEmail == "") {
         //No email in the request body
         res.status(400).send({
-            "message": "No email address was provided"
+            "message": "No email address was provided."
         })
     } else {
         request('http://www.mocky.io/v2/5808862710000087232b75ac', (error, response, body) => {
@@ -37,7 +37,6 @@ server.post('/login', (req, res) => {
                         "role": clientData["role"]
                     }, "jwtpassword", (err, token) => {
                         if (err) throw (err)
-                        console.log("Login successful.")
                         res.status(200).send({
                             "token": token,
                         })
@@ -80,7 +79,7 @@ server.get("/clientID/:id", (req, res) => {
                 })
             } else {
                 res.status(404).send({
-                    "message": "Client not found. Please check the ID is correct"
+                    "message": "Client not found. Please check the ID is correct."
                 })
             }
         }
@@ -103,7 +102,7 @@ server.get("/clientName/:name", (req, res) => {
                 })
             } else {
                 res.status(404).send({
-                    "message": "Client not found. Please check the name is correct"
+                    "message": "Client not found. Please check the name is correct."
                 })
             }
         }
@@ -124,107 +123,136 @@ server.get("/policies", (req, res) => {
 //Get list of policies linked to a client name
 server.get("/policiesOfClient/:clientName", (req, res) => {
 
-    let authToken = req.headers["authorization"];
-    authToken = authToken.replace("Bearer ", "")
-
-    jwt.verify(authToken, "jwtpassword", (err, decoded) => {
-        if (err) throw err
-        else {
-            //User is valid, but only admin can proceed
-            if (decoded["role"] != "admin") {
-                res.status(401).send({
-                    "message": "Unauthorized user"
-                })
-            } else {
-                request('http://www.mocky.io/v2/5808862710000087232b75ac', (error, response, clientsBody) => {
-                    if (error) throw error
-                    else {
-                        let allClients = JSON.parse(clientsBody).clients;
-                        let _name = req.params.clientName;
-                        let clientData = allClients.filter(client => client["name"] === _name)[0];
-
-                        if (clientData) {
-                            request('http://www.mocky.io/v2/580891a4100000e8242b75c5', (error, response, policiesBody) => {
-                                if (error) throw error
-                                else {
-                                    let allPolicies = JSON.parse(policiesBody).policies;
-                                    let policiesOfUser = allPolicies.filter(policy => policy["clientId"] === clientData["id"]);
-
-                                    if (policiesOfUser.length == 0) {
-                                        //That client has no policies linked to them
-                                        res.status(404).send({
-                                            "message": "No policies linked to this client were found. Please check the name is correct"
-                                        })
-                                    } else {
-                                        res.status(200).send({
-                                            "data": policiesOfUser
-                                        })
-                                    }
+    try {
+        let authToken = req.headers["authorization"];
+        if (!authToken) {
+            res.status(401).send({
+                "message": "Unauthorized user. Authentication token is required."
+            })
+        } else {
+            authToken = authToken.replace("Bearer ", "")
+    
+            jwt.verify(authToken, "jwtpassword", (err, decoded) => {
+                if (err) throw "Unauthorized user. Token provided is not valid."
+                else {
+                    //User is valid, but only admin can proceed
+                    if (decoded["role"] != "admin") {
+                        res.status(401).send({
+                            "message": "Unauthorized user."
+                        })
+                    } else {
+                        request('http://www.mocky.io/v2/5808862710000087232b75ac', (error, response, clientsBody) => {
+                            if (error) throw error
+                            else {
+                                let allClients = JSON.parse(clientsBody).clients;
+                                let _name = req.params.clientName;
+                                let clientData = allClients.filter(client => client["name"] === _name)[0];
+    
+                                if (clientData) {
+                                    request('http://www.mocky.io/v2/580891a4100000e8242b75c5', (error, response, policiesBody) => {
+                                        if (error) throw error
+                                        else {
+                                            let allPolicies = JSON.parse(policiesBody).policies;
+                                            let policiesOfUser = allPolicies.filter(policy => policy["clientId"] === clientData["id"]);
+    
+                                            if (policiesOfUser.length == 0) {
+                                                //That client has no policies linked to them
+                                                res.status(404).send({
+                                                    "message": "No policies linked to this client were found. Please check the name is correct."
+                                                })
+                                            } else {
+                                                res.status(200).send({
+                                                    "data": policiesOfUser
+                                                })
+                                            }
+                                        }
+                                    });
+                                } else {
+                                    res.status(404).send({
+                                        "message": "Client not found. Please check the name is correct."
+                                    })
                                 }
-                            });
-                        } else {
-                            res.status(404).send({
-                                "message": "Client not found. Please check the name is correct"
-                            })
-                        }
+                            }
+                        });
                     }
-                });
-            }
+                }
+            })
         }
-    })
+    }
+    catch (e) {
+        res.status(401).send({
+            "message": e
+        });
+    }
+    
 })
 
 //Get client data linked to a policy id
 server.get("/policyClient/:policyId", (req, res) => {
-    let authToken = req.headers["authorization"];
-    authToken = authToken.replace("Bearer ", "")
 
-    jwt.verify(authToken, "jwtpassword", (err, decoded) => {
-        if (err) throw err
-        else {
-
-            //User is valid, but only admin can proceed
-            if (decoded["role"] != "admin") {
-                res.status(401).send({
-                    "message": "Unauthorized user"
-                })
-            } else {
-                request('http://www.mocky.io/v2/580891a4100000e8242b75c5', (error, response, policiesBody) => {
-
-                    if (error) throw error
-                    else {
-                        let allPolicies = JSON.parse(policiesBody).policies
-                        let _id = req.params.policyId;
-                        let policyData = allPolicies.filter(policy => policy["id"] === _id)[0];
-
-                        if (policyData) {
-                            request('http://www.mocky.io/v2/5808862710000087232b75ac', (error, response, clientsBody) => {
-                                if (error) throw error
-                                else {
-                                    let allClients = JSON.parse(clientsBody).clients
-                                    let clientData = allClients.filter(client => client["id"] === policyData["clientId"])[0];
-                                    if (clientData) {
-                                        res.status(200).send({
-                                            "data": clientData
-                                        })
-                                    } else {
-                                        //That policy has no clients linked to them
-                                        res.status(404).send({
-                                            "message": "No clients linked to this policy were found. Please check the policy Id is correct"
-                                        })
-                                    }
+    try {
+        let authToken = req.headers["authorization"];
+        if (!authToken) {
+            res.status(401).send({
+                "message": "Unauthorized user. Authentication token is required."
+            })
+        } else {
+            authToken = authToken.replace("Bearer ", "")
+    
+            jwt.verify(authToken, "jwtpassword", (err, decoded) => {
+                if (err) throw "Unauthorized user. Token provided is not valid."
+                else {
+    
+                    //User is valid, but only admin can proceed
+                    if (decoded["role"] != "admin") {
+                        res.status(401).send({
+                            "message": "Unauthorized user."
+                        })
+                    } else {
+                        request('http://www.mocky.io/v2/580891a4100000e8242b75c5', (error, response, policiesBody) => {
+    
+                            if (error) throw error
+                            else {
+                                let allPolicies = JSON.parse(policiesBody).policies
+                                let _id = req.params.policyId;
+                                let policyData = allPolicies.filter(policy => policy["id"] === _id)[0];
+    
+                                if (policyData) {
+                                    request('http://www.mocky.io/v2/5808862710000087232b75ac', (error, response, clientsBody) => {
+                                        if (error) throw error
+                                        else {
+                                            let allClients = JSON.parse(clientsBody).clients
+                                            let clientData = allClients.filter(client => client["id"] === policyData["clientId"])[0];
+                                            if (clientData) {
+                                                res.status(200).send({
+                                                    "data": clientData
+                                                })
+                                            } else {
+                                                //That policy has no clients linked to them
+                                                res.status(404).send({
+                                                    "message": "No clients linked to this policy were found. Please check the policy Id is correct."
+                                                })
+                                            }
+                                        }
+                                    })
+                                } else {
+                                    res.status(404).send({
+                                        "message": "Policy not found. Please check the policy Id is correct."
+                                    })
                                 }
-                            })
-                        } else {
-                            res.status(404).send({
-                                "message": "Policy not found. Please check the policy Id is correct"
-                            })
-                        }
+                            }
+                        })
                     }
-                })
-            }
+                }
+            })
         }
-    })
+    }
+    catch (e) {
+        res.status(401).send({
+            "message": e
+        });
+    }
+    
 })
 
 server.listen(3000, () => {
